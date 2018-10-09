ef0656a3e5e69b88683511dd9145c12af892f90d
Le Do Tan <natodel@gmail.com>
2018-10-09 16:00:51 +0700

    update testscript

--- src/selenium_api/Topic_04_TextboxTextAreaDropdownlist.java
+++ src/selenium_api/Topic_04_TextboxTextAreaDropdownlist.java
@@ -13,7 +13,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class Topic04_TextboxTextAreaDropdownlist {
+public class Topic_04_TextboxTextAreaDropdownlist {
 
 	// Ini driver
 	WebDriver driver;
